121ac44d873fa8ce96ef41af906e0e855a2da2b8
Shreyas-V06 <shreyasvakkiyel@gmail.com>
2026-06-12 15:57:02 +0530

    removal service methods

--- services/redis_queue.py
+++ services/redis_queue.py
@@ -10,12 +10,26 @@ async def addPlayerMetadata(player:User):
     timestamp = int(time.time())
     await r.hset(key,player_id,timestamp)
 
-async def addPlayerToQueue(player:User):
+async def removePlayerMetadata(player:User):
+    key = "coden:queue:metadata"
+    player_id = player.user_id
+    await r.hdel(key,player_id)
+
+async def addPlayer(player:User):
     key = "coden:queue"
     player_id = player.user_id
     player_score = player.score
     await r.zadd(key,{player_id:player_score})
     await addPlayerMetadata(player)
+
+async def removePlayer(player:User):
+    key = "coden:queue"
+    player_id = player.user_id
+
+    await r.zrem(key,player_id)
+    await removePlayerMetadata(player)
+
+
 
     
     
