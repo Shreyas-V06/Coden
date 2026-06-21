4d46a1d933f400a18cac759716c6748e957e9a83
Shreyas-V06 <shreyasvakkiyel@gmail.com>
2026-06-21 07:18:24 +0530

    added redis pipelining for fixing race conditions

--- services/redis_queue.py
+++ services/redis_queue.py
@@ -1,32 +1,24 @@
 import redis.asyncio as redis
-from models.domain import User
 import time
 
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
-#TODO: 
-async def addPlayerMetadata(player:User):
-    key = "coden:queue:metadata"
-    player_id = player.user_id
+metadata_key = "coden:queue:metadata"
+queue_key = "coden:queue"
+
+async def addPlayer(player_id:str,player_score:float):
     timestamp = int(time.time())
     player_metadata = {player_id:timestamp}
-    await r.zadd(key,player_metadata)
+    async with r.pipeline(transaction=True) as pipe:
+        pipe.zadd(metadata_key,player_metadata)
+        pipe.zadd(queue_key,{player_id:player_score})
+        await pipe.execute()
 
-async def removePlayerMetadata(player_id):
-    key = "coden:queue:metadata"
-    await r.zrem(key,player_id)
-
-async def addPlayer(player:User):
-    key = "coden:queue"
-    player_id = player.user_id
-    player_score = player.score
-    await r.zadd(key,{player_id:player_score})
-    await addPlayerMetadata(player)
-
-async def removePlayer(player_id):
-    key = "coden:queue"
-    await r.zrem(key,player_id)
-    await removePlayerMetadata(player_id)
+async def removePlayer(player_id:str):
+    async with r.pipeline(transaction=True) as pipe:
+        pipe.zrem(queue_key,player_id)
+        pipe.zrem(metadata_key,player_id)
+        await pipe.execute()
 
 
 
